6ca6e032ff005582f327946ed86a96ce2f1a2508
zuplace <zuplace@gmail.com>
2020-06-03 00:10:51 -0500

    added endpoint /mercury

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/Mercury', function (req, res) {
+  res.send('Hello Mercury!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
